6539b99b8cb1e66532cc6c723956546e016e28fc
eli <eli@newstartmobile.com>
2024-08-10 13:24:11 -0400

    Initial Commit

---
+++ pico cam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pico cam
+//
+//  Created by Eli Mellen on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pico cam/pico_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  pico_camApp.swift
+//  pico cam
+//
+//  Created by Eli Mellen on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct pico_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
